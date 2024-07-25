60ce99162494a317bd7a00e517efaf892af682e9
Huysupercool666 <huyTD30112001@gmail.com>
2024-07-25 18:34:33 +0700

    Huy | add edit, delete, toggle complete feature

--- todoAppOOP/script.js
+++ todoAppOOP/script.js
@@ -31,7 +31,7 @@ tasks.prototype.render = function () {
   taskList.innerHTML = ""
   taskList.innerHTML = this.listTask
     .map((item) => {
-      return ` <li><input type="checkbox" ${item.completed ? checked : ""}>
+      return ` <li><input onchange="newTaskList.toggleCompleted(${item.id})" type="checkbox" ${item.completed ? checked : ""}>
         <span>${item.name}</span>
         <button class="button" onclick="newTaskList.editTask(${item.id})">Edit</button>
         <button class="button" onclick="newTaskList.deleteTask(${item.id})">Delete</button>
@@ -44,22 +44,39 @@ tasks.prototype.cancelTask = function () {
   document.getElementById("inputValue").value = "";
 };
 
-task.prototype.deleteTask = function () {
-  arrayList.map(() => arrayList[i].id === id);
-    arrayList.splice(i, 1); 
-    task.prototype.filterTask();
+tasks.prototype.deleteTask = function (id) {
+  this.listTask.filter((item, index) => item.id === id ? this.listTask.splice(index, 1):"");
+    console.log(this.listTask)
+    this.render();
 }
 
-tasks.prototype.editTask = function () {};
+tasks.prototype.editTask = function (id) {
+  const task = this.listTask.find((task) => task.id === id);
+  const newTaskName = prompt("Change your task here", task.name);
+  if (newTaskName !== null && newTaskName.trim() !== "") {
+    const isTaskExist = this.listTask.find(
+      (task) => task.name === newTaskName.trim()
+    );
+    if (isTaskExist) {
+      alert("Already have this task !");
+    } else {
+      task.name = newTaskName.trim();
+      this.render();
+    }
+  }
+};
 
 tasks.prototype.filterTask = function () {};
 
-tasks.prototype.toggleCompleted = function () {};
+tasks.prototype.toggleCompleted = function (id) {
+  this.listTask.forEach((item) => item.id === id ? item.completed = !item.completed:'')
+  console.log(this.listTask); 
+};
 
 tasks.prototype.sortTask = function () {};
 
 let newTaskList = new tasks();
-let id = 0;
+
 // DOM
 document.getElementById("addButton").addEventListener("click", function () {
   newTaskList.addTask();
