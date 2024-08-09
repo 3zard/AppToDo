6947147d8c33daddf2ba6ad4ffbc5bb255138336
Tran Hoai Phong <thp080203@mail.com>
2024-08-09 16:02:10 +0700

    Phong | add utils.js

---
+++ backendserver/utils.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+function writeFile(filename, content) {
+  fs.writeFile(filename, content, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+}
+
+function getBody(request) {
+  return new Promise((resolve, reject) => {
+      let body = '';
+
+      request.on('data', chunk => {
+          body += chunk.toString();
+      });
+
+      request.on('end', () => {
+          resolve(body);
+      });
+
+      request.on('error', error => {
+          reject(error);
+      });
+  });
+}
+
+module.exports = {
+  getBody,
+};
